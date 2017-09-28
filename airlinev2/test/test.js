79d46d75db6e4ddc0a2ce397227d3d32b02ca361
acloudfan <rajeev@Rajeevs-MacBook-Pro.local>
2017-09-28 08:10:04 -0400

    aa

---
+++ airlinev2/test/test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const AdminConnection = require('composer-admin').AdminConnection;
+// const BrowserFS = require('browserfs/dist/node/index');
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+const BusinessNetworkDefinition = require('composer-common').BusinessNetworkDefinition;
+// const path = require('path');
+
+// require('chai').should();
+
+const bfs_fs = BrowserFS.BFSRequire('fs');
+const NS = 'org.hyperledger_composer.marbles';
+
+describe('AirlineV2', () => {
+
+    console.log("Test invoked")
+})
+
